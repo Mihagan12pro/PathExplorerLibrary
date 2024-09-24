16aff7a2b1dc98141b776a3a2b3950f333ae8dee
Mihagan12pro <mikhail.kargin.05@mail.ru>
2024-09-24 21:01:51 +0300

    The FilePath class can get project's directory.

--- Explorer.cs
+++ Explorer.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
 namespace PathExplorerLibrary
 {
     public class Explorer
     {
-        public Explorer()
+        public Explorer(FilePath filePath)
         {
             
         }
@@ -16,6 +17,48 @@ namespace PathExplorerLibrary
 
     public sealed class FilePath
     {
+        public readonly string FileName;
 
+        public readonly string ProjectName;
+
+        public readonly  string ProjectDir;
+
+
+        private string path;
+        public string Path
+        {
+            get
+            {
+                if (!File.Exists(path))
+                {
+                   DirectoryInfo projectDirectory = new DirectoryInfo(ProjectDir);
+
+
+                }
+
+
+                return path;
+            }
+            private set
+            {
+                path = value;
+            }
+        }
+
+        public FilePath(string FileName,string ProjectName)
+        {
+            this.FileName = FileName;
+            this.ProjectName = ProjectName;
+
+            string ProjectExe = new FileInfo(ProjectName).FullName;
+
+         
+            DirectoryInfo debugDir = new DirectoryInfo(ProjectExe.Replace(ProjectName,""));
+          
+            DirectoryInfo binDir = new DirectoryInfo(debugDir.Parent.FullName);
+
+          
+            ProjectDir = new DirectoryInfo(binDir.Parent.FullName).FullName;
+        }
     }
 }
