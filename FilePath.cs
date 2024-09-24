0c43787e596e1087651f4f665f04e4c687310d65
Mihagan12pro <mikhail.kargin.05@mail.ru>
2024-09-24 21:07:18 +0300

    The FilePath class has its own .cs file.

---
+++ FilePath.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+namespace PathExplorerLibrary
+{
+
+    public class FilePath
+    {
+        public readonly string FileName;
+
+        public readonly string ProjectName;
+
+        public readonly string ProjectDir;
+
+
+        private string path;
+        public string Path
+        {
+            get
+            {
+                if (!File.Exists(path))
+                {
+                    DirectoryInfo projectDirectory = new DirectoryInfo(ProjectDir);
+
+
+                }
+
+
+                return path;
+            }
+            private set
+            {
+                path = value;
+            }
+        }
+
+        public FilePath(string FileName, string ProjectName)
+        {
+            this.FileName = FileName;
+            this.ProjectName = ProjectName;
+
+            string projectExe = new FileInfo(ProjectName).FullName;
+
+
+            DirectoryInfo debugDir = new DirectoryInfo(projectExe.Replace(ProjectName, ""));
+
+            DirectoryInfo binDir = new DirectoryInfo(debugDir.Parent.FullName);
+
+
+            ProjectDir = new DirectoryInfo(binDir.Parent.FullName).FullName;
+        }
+    }
+}
