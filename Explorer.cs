4f4f26d4b38066a9af77d77666fd188250239ab0
Mihagan12pro <mikhail.kargin.05@mail.ru>
2024-10-04 08:39:26 +0300

    The Exceptions static class was created.

--- Explorer.cs
+++ Explorer.cs
@@ -9,25 +9,41 @@ namespace PathExplorerLibrary
 {
     public abstract class AbstractExplorer
     {
+        protected readonly List<PathMaster> pathMasters;
         protected readonly Exception eDublicateObjects = new Exception("Two objects that work with one file's path were found!");
+
+        protected abstract void UniquePaths();
         public AbstractExplorer(PathMaster filePath)
         {
-            
+            pathMasters = new List<PathMaster>();
         }
     }
 
     public class Example : AbstractExplorer
     {
         public readonly PathMaster filePath1,filePath2;
+        protected override void UniquePaths()
+        {
+            var filePaths = from filePath in pathMasters where filePath.FilePath!= null select filePath.FilePath;
+
+
+            if (file)
+        }
+
+
         public Example(PathMaster filePath1,PathMaster filePath2) : base(filePath1)
         {
             this.filePath1 = filePath1;
             this.filePath2 = filePath2;
 
-            if (filePath1.FilePath == filePath2.FilePath)
-            {
-                throw eDublicateObjects;
-            }
+
+            pathMasters.Add(filePath1);
+            pathMasters.Add(filePath2);
+
+            //if (filePath1.FilePath == filePath2.FilePath)
+            //{
+            //    throw eDublicateObjects;
+            //}
         }
     }
 }
